0b5de5cb1a0a3f6f385260baf5c52009109ce5e7
Sanketdhami <sanketdhami@gmail.com>
2016-01-15 16:51:15 -0800

    login/sign up

--- Auth/urls.py
+++ Auth/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name = 'index'),
+	url(r'^(?P<isRedirect>[a-zA-Z]*)$', views.index, name = 'index'),
 	url(r'^details$', views.details, name = 'details'),
 ]
--- Auth/views.py
+++ Auth/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader, RequestContext
 from . models import Users
+from django.core.urlresolvers import reverse
 
 # Create your views here.
-def index(request):
-	context = RequestContext(request, {'useridLabel':'Username:', 'passwordLabel':'Password:',})
-	return render(request, 'Auth/index.html', context)
+def index(request,isRedirect):
+	if isRedirect =='':
+		context = RequestContext(request, {'useridLabel':'Username:', 'passwordLabel':'Password:'})
+		return render(request, 'Auth/index.html', context)	
+	else:
+		context = RequestContext(request, {'useridLabel':'Username:', 'passwordLabel':'Password:', 'type':type(isRedirect)})
+		return render(request, 'Auth/index.html', context)
 
 def details(request):
 	userid = request.POST['username']
-	value = get_object_or_404(Users, pk=userid)
-	return HttpResponse("Hello %s " % value)
+	f_name = request.POST['firstname']
+	l_name = request.POST['lastname']
+	name = f_name + " " + l_name
+	email = request.POST['email']
+	passwrd = request.POST['password']
+	user = Users(userid,name,email,'1234','dhaval.jpg')
+	#user.save()
+	try:
+		value = Users.objects.get(pk=userid)
+	except Users.DoesNotExist:
+		return HttpResponse("Hello %s" % value)	
+	else:
+		return HttpResponseRedirect(reverse('Auth:index',args = ('True',)))
+	
