e74ad933c40ae189164897b7e9ee3fc673183fe6
Dhruven Vora <dhruven91@gmail.com>
2016-01-16 15:45:38 -0800

    added login page

--- Auth/views.py
+++ Auth/views.py
@@ -6,23 +6,37 @@ from django.core.urlresolvers import reverse
 
 # Create your views here.
 def index(request):
-		context = RequestContext(request, {'useridLabel':'Username:', 'passwordLabel':'Password:'})
-		return render(request, 'Auth/index.html', context)
+	context = RequestContext(request, {'useridLabel':'Username:', 'passwordLabel':'Password:'})
+	return render(request, 'Auth/index.html', context)
+		
+def login(request):
+	context = RequestContext(request)
+	return render(request, 'Auth/login.html', context)
 
 def details(request):
-	userid = request.POST['username']
-	f_name = request.POST['firstname']
-	l_name = request.POST['lastname']
-	name = f_name + " " + l_name
-	email = request.POST['email']
-	passwrd = request.POST['password']
-	user = Users(userid,name,email,'1234','dhaval.jpg')
-	value= None
-	try:
-		value = Users.objects.get(pk=userid)
-	except Users.DoesNotExist:
-		user.save()
-		context = RequestContext(request, {'userid':user.name})
-		return render(request, 'Auth/details.html',context)	
+	referer_url = request.META.get('HTTP_REFERER') #HTTP_REFERER contains the URL of the calling view.
+	if referer_url is not None:
+		if 'login' in referer_url:
+			userid = request.POST['username']
+			passwrd = request.POST['password']
+			#TODO instead of printing the string, redirect the page to Tasks/index
+			return HttpResponse("You are logged in %s. " % userid)
+		elif 'signup' in referer_url:
+			userid = request.POST['username']
+			f_name = request.POST['firstname']
+			l_name = request.POST['lastname']
+			name = f_name + " " + l_name
+			email = request.POST['email']
+			passwrd = request.POST['password']
+			user = Users(userid,name,email,'1234','dhaval.jpg')
+			value= None
+			try:
+				value = Users.objects.get(pk=userid)
+			except Users.DoesNotExist:
+				user.save()
+				context = RequestContext(request, {'userid':user.name})
+				return render(request, 'Auth/details.html',context)	
+			else:
+				return HttpResponseRedirect(reverse('Auth:index'))
 	else:
 		return HttpResponseRedirect(reverse('Auth:index'))
