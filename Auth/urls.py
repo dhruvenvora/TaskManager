d497ad478b9b42d8e01639a9812dbe581067bd7e
Dhruven Vora <dhruven91@gmail.com>
2016-01-16 13:47:00 -0800

    Added bootstrap library, changed layout of index.html, changed URL from login to signup

--- Auth/urls.py
+++ Auth/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^login/*$', views.index, name = 'index'),
+	url(r'^signup/*$', views.index, name = 'index'),
 	url(r'^details/*$', views.details, name = 'details'),
 ]
