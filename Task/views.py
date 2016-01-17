cbbfdf65552c38793977f7b813dd3550ac43a42a
Sanketdhami <sanketdhami@gmail.com>
2016-01-17 01:23:09 -0800

    added index+base+addtask.htmls

--- Task/views.py
+++ Task/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader, RequestContext
+from . models import Tasks
+
 
 # Create your views here.
+def index(request):
+	return render(request, 'Task/index.html')
+
+def addtask(request):
+	return render(request, 'Task/addtask.html')
--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^Auth/', include('Auth.urls', namespace='Auth')),
+	url(r'^Task/', include('Task.urls', namespace='Task')),
 ]
