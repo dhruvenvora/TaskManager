aa870f651d412f719ee1a442a76d444d34b015d1
Sanketdhami <sanketdhami@gmail.com>
2016-01-18 01:07:37 -0800

    modified .html file

--- Task/models.py
+++ Task/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from decimal import Decimal
 
 from django.utils import timezone
 # Create your models here.
@@ -8,10 +9,10 @@ class Tasks(models.Model):
 	taskid = models.CharField(max_length = 100, primary_key=True) #primary key
 	title = models.CharField(max_length = 100)
 	description = models.CharField(max_length = 250)
-	creation_date = models.DateTimeField(default = timezone.now,blank = True)
+	start_date = models.DateTimeField(default = timezone.now,blank = True)
 	due_date = models.DateTimeField(default = timezone.now,blank = True)
-	is_rotation = models.BooleanField(default = 0)
-	remind_before = models.DateTimeField(default = timezone.now,blank = True)
+	repeat = models.BooleanField(default = 0)
+	set_reminder_before = models.DecimalField(max_digits=20,decimal_places=4,default=Decimal('0.0000'))
 	
 	
 	def __str__(self):
