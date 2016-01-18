6648566564b1808b3f1a1c3df6cc71a7885f9b74
Sanketdhami <sanketdhami@gmail.com>
2016-01-17 23:44:31 -0800

    modified views.py

--- Task/views.py
+++ Task/views.py
@@ -9,13 +9,15 @@ def index(request):
 	return render(request, 'Task/index.html')
 
 def addtask(request):
-	taskid = '5'
+	taskid = '12'
 	title = request.POST['title']
 	description = request.POST['description']
-	start_date = request.POST['start_date']
-	due_date = request.POST['due_date']
+	x = request.POST['start_date']
+	start_date = x + ' 00:00:00'
+	y = request.POST['due_date']
+	due_date = y + ' 00:00:00'
 	set_reminder_before = request.POST['time_counter']
 	repeat = request.POST['repeat_checkbox']
 	task = Tasks(taskid,title,description,start_date,due_date,repeat,set_reminder_before)
 	task.save()
-	return HttpResponse('Task added')
+	return HttpResponse(title)
