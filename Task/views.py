e6050a30e630b38cbf7e020fc53dcc3403a698bc
Sanketdhami <sanketdhami@gmail.com>
2016-01-18 01:04:16 -0800

    modified views.py

--- Task/views.py
+++ Task/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader, RequestContext
 from . models import Tasks
 
-
+count = 0
 # Create your views here.
 def index(request, userid):
-	context = RequestContext(request, {'userid':userid})
+	task_list = Tasks.objects.all() 
+	context = RequestContext(request, {'userid':userid,'task_list_label':task_list})
 	return render(request, 'Task/index.html', context)
 
 def addtask(request):
@@ -21,4 +22,10 @@ def addtask(request):
 	repeat = request.POST['repeat_checkbox']
 	task = Tasks(taskid,title,description,start_date,due_date,repeat,set_reminder_before)
 	task.save()
-	return HttpResponse(title)
+	global count 
+	count = counter(count)
+	return render(request, 'Task/index.html', context)
+
+def counter():
+	count = count  + 1 ;
+	return count
